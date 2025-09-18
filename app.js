f054535539aa88c7a7a9dbbf251815ea78026be9
Kissi Yao <41991890+yaokissi@users.noreply.github.com>
2025-09-18 21:05:46 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+import * as THREE from 'three';
+
+export default class Sketch {
+  constructor(options) {
+    this.render();
+    this.time = 0;
+
+  }
+  render() {
+    this.time += 0.05;
+    console.log(this.time);
+   requestAnimationFrame(this.render.bind(this));
+  }
+}
+new Sketch();
+
+
+    
+/*
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+let container = document.getElementById('container');
+container.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render( scene, camera );
+
+}
+  */
